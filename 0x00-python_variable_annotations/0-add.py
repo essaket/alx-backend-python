7da34077d63e3cd9385d4eb51d637f8eb2ac562a
essaket <hichamessaket17@gmail.com>
2024-08-01 06:08:13 -0700

    Writing task 0

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3
-""" Module documentation """
+#!/usr/bin/env paython3
+"""0. Basic annotations - add"""
 
 
 def add(a: float, b: float) -> float:
-    """Func doc"""
+    """A function takes two floats and return a float"""
     return a + b
