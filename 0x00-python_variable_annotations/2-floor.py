7cdc325cee255ad15c391ec3fd100465e39e3eb7
essaket <hichamessaket17@gmail.com>
2024-08-01 06:37:36 -0700

    Writing task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""2. Basic annotations - floor"""
+import math
+
+
+def floor(n: float) -> int:
+    """A function that return the floor of the float"""
+    return math.floor(n)
