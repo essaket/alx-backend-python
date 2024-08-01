ffd50d4b9f544c8c35acfe7d8577368bc25cbbfd
essaket <hichamessaket17@gmail.com>
2024-08-01 06:47:43 -0700

    Writing task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""3. Basic annotations - to string"""
+
+
+def to_str(n: float) -> str:
+    """A function takes a float and return string representation of it"""
+    return str(n)
